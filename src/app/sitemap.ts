bf02f85acef468070e88f3e870988b7ed7040ff9
Jake Martin <jake.alderman@gmail.com>
2026-08-09 10:03:01 +1000

    Add /privacy to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,6 +14,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     '/blog',
     '/board',
     '/contact',
+    '/privacy',
   ].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
